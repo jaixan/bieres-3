d6f9a83634efa08a72035f8e6fa0bc90fd0a7722
Etienne Rivard <etienne@kerzo.ca>
2024-11-01 08:33:36 -0400

    préparation netlify

---
+++ functions/api.ts
@@ -0,0 +1,16 @@
+/**
+ * Point d'entrée pour les fonctions de Netlify
+ */
+
+import app from '../src/server';
+
+import dotenv from 'dotenv';
+
+import serverless from 'serverless-http';
+
+// *** Variables d'environnement ***
+
+dotenv.config();
+
+// *** Le handler requis par Netlify **
+export const handler = serverless(app);
